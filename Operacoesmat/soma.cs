99e6ccc675c786f9aa74074d6ff72228bc41247b
Leandro Costa <lcostasantos1987@gmail.com>
2022-01-02 23:31:43 -0300

    Create soma.cs
    soma

---
+++ Operacoesmat/soma.cs
@@ -0,0 +1,29 @@
+using System;
+
+{
+
+
+   static void Main();
+
+
+   {
+
+     int num1;
+     int num2;
+     int soma;
+
+    
+
+       Console.WriteLine("Digite o primeiro numero");
+       num1 = int.Parse(Console.ReadLine());
+
+       Console.WriteLine("Digite o segundo Numero");
+       Num2 = int.Parse(Console.ReadLine());
+       soma = num1 + num2;
+  
+       Console.WriteLine("A soma dos numeros é: " + soma);
+   }
+
+}
+
+

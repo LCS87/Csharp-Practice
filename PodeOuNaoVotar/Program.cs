28b38fe93b4c3d84ef9fcce0aa92ae5c45826e79
Leandro Costa <lcostasantos1987@gmail.com>
2022-04-19 20:53:28 -0300

    Pode votar ou não.
    Pode votar ou não.

---
+++ PodeOuNaoVotar/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+int eleitor;
+
+Console.WriteLine("Digite a idade do eleitor: ");
+eleitor = Convert.ToInt32(Console.ReadLine());
+
+   if(eleitor  < 18)
+    {
+    Console.WriteLine("Desculpa mas voce nã opode votar!");   
+    }
+   else
+   {
+    Console.WriteLine("Parabens! voce esta apto para votar.");
+    }
+
+   Console.ReadLine();

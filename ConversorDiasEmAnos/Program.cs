5b3aaa72fc306602e9b52963218b4b09b1e4550e
Leandro Costa <lcostasantos1987@gmail.com>
2022-04-19 21:57:24 -0300

    Conversor de dias em semanas e anos
    Conversor de dias em semanas e anos

---
+++ ConversorDiasEmAnos/Program.cs
@@ -0,0 +1,15 @@
+﻿int dias, anos, semanas;
+
+    Console.WriteLine("Digite os Dias: ");
+    dias = Convert.ToInt32(Console.ReadLine());
+
+   
+    anos = (dias / 365);
+    semanas = (dias % 365) / 7;
+    dias = dias - ((anos * 365) + (semanas * 7));
+
+Console.WriteLine("Anos    : "+ anos );
+    Console.WriteLine("Semanas : "+ semanas);
+    Console.WriteLine("Dias    : "+ dias);
+
+   Console.ReadKey();

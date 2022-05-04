cbdeb3aef6cbfe36efab0ad0561b1aed2be1ce78
Leandro Costa <lcostasantos1987@gmail.com>
2022-05-04 19:24:07 -0300

    up
    up

---
+++ CalculoFuncaoMatematica/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class Exercise6
+{
+    public static void Main()
+    {
+        int x, y;
+
+        Console.WriteLine("x = y² - 2y +1");
+        Console.WriteLine();
+
+        for (y = -5; y <= 5; y++)
+        {
+            x = y * y - 2 * y + 1;
+            Console.WriteLine(
+                "y = {0} ; x=({0})² - 2*({0}) +1 = {1}",
+                y, x);
+        }
+    }
+}
---
+++ PerimetroCirculo/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+public class Exercicio
+{
+    public static void Main()
+    {
+
+        double r, per_cir;
+        double PI = 3.14;
+        Console.WriteLine("Digite o raio do circulo : ");
+        r = Convert.ToDouble(Console.ReadLine());
+        per_cir = 2 * PI * r;
+        Console.WriteLine("O Perimento é : {0}", per_cir);
+        Console.Read();
+    }
+}
